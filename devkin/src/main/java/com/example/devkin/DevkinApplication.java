e12fd7bd3689915faf4e908ee557824b10cf84cc
Ibrahim <ibrahim0ineizeh@gmail.com>
2024-10-01 18:38:11 +0300

    Proejct

---
+++ devkin/src/main/java/com/example/devkin/DevkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.devkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevkinApplication.class, args);
+	}
+
+}
---
+++ devkin/src/test/java/com/example/devkin/DevkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
